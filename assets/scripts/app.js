160c475721b33efe31d973f52a927924a5644967
neerajkashetty <neeraj.kashetty29@gmail.com>
2022-09-12 00:24:31 +0530

    If-else

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,50 @@
 const ATTTACK_VALUE = 10;
+const Monster_Attack = 13;
+const DOUBLE_ATTACK = 30;
+const HEALTH_UP = 30;
 
 let chosenMaxLife = 100;
 let currentmonsterHealth = chosenMaxLife;
+let playerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
-console.log('did');
-function attack() {
-    console.log('clicked');
-    const damage = dealMonsterDamage(ATTTACK_VALUE);
-    currentmonsterHealth -= damage;
+
+function endGame () {
+    const playerdamage = dealPlayerDamage(Monster_Attack);
+    playerHealth -= playerdamage;
+    if (currentmonsterHealth <= 0 && playerHealth > 10){
+        alert('Your the winner');
+    } else if (playerHealth <= 0 && currentmonsterHealth < 0){
+        alert('Your the Loser')
+    }
 }
 
+function Attacks (type){
+    let maxdamage;
+    if (type === 'Attack'){
+        maxdamage = ATTTACK_VALUE
+    } else {
+        maxdamage = DOUBLE_ATTACK
+    }
+    const damage = dealMonsterDamage(maxdamage);
+    currentmonsterHealth -= damage;
+    endGame();
+}
 
+function attack() { 
+    Attacks('Attack');
+}
+
+function StrongAtck () {
+    Attacks('Strong_Attack');
+}
+
+function HealMee () {
+    increasePlayerHealth(HEALTH_UP);
+    playerHealth += HEALTH_UP;
+    endGame();
+}
+
+healBtn.addEventListener('click', HealMee);
+strongAttackBtn.addEventListener('click', StrongAtck);
 attackBtn.addEventListener('click', attack);
